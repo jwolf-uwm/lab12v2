25428c2341eaa796aa1d2705d9c924bc5662c21c
Jeffrey Wolf <lwolf@uwm.edu>
2019-04-17 11:59:40 -0500

    model crap

--- giftReg/models.py
+++ giftReg/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_username = models.CharField(max_length=50)
+    user_email = models.CharField(max_length=100)
+    user_password = models.CharField(max_length=50)
+
+
+class Gift(models.Model):
+    gift_name = models.CharField(max_length=100)
