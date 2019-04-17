c540d95046d522fbe72fdc58173f52f6f8c097f9
Jeffrey Wolf <lwolf@uwm.edu>
2019-04-17 13:55:57 -0500

    reg crap

--- giftReg/views.py
+++ giftReg/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+
 from giftReg.models import User
 
 # Create your views here.
@@ -19,6 +20,26 @@ class Registration(View):
     def post(self, request):
         user = User(user_email=request.POST['userEmail'], user_username=request.POST['userName'],
                     user_password=request.POST['userPassword'])
-        user.save()
 
-        return render(request, "main/registration.html")
+        does_exist = True
+
+        try:
+            check_email = User.objects.get(user_email=user.user_email)
+            does_exist = True
+        except User.DoesNotExist:
+            does_exist = False
+
+            try:
+                check_username = User.objects.get(user_username=user.user_username)
+                does_exist = True
+            except User.DoesNotExist:
+                does_exist = False
+
+
+
+        if not does_exist:
+            user.save()
+            return render(request, "main/registration.html", {"message": "Success!"})
+        else:
+            return render(request, "main/registration.html", {"message": "Failure, username or email address already "
+                                                                         + "taken"})
